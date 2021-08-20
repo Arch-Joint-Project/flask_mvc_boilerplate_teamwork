3397b65be040d8f00c2f275985fdd47e02cfbf4d
Michael Asumadu <michaelasumadu1@gmail.com>
2021-08-20 16:52:31 +0000

    test database addition

---
+++ users_202108201601.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.users ("First Name","Last Name","Username","Email","Age","Date of Birth","Password") VALUES
+	 ('desmond','asumadu','rseeafdfadfa','readadffaasfas@sltest.com',3,'2020-08-20','slkasdfsjd'),
+	 ('mihcael','asumadu','rseeafaasdfdsfdfadfa','readadffaaafdsadffas@sltest.com',3,'2020-08-20','slkasdfsjd');
