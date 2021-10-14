a44aa70ea3027b27f689bf5e0d29bbd33600fa42
Michael Asumadu <michaelasumadu1@gmail.com>
2021-10-14 10:04:02 +0000

    error handling for email_notification

--- app/services/email_service.py
+++ app/services/email_service.py
@@ -2,13 +2,17 @@ from app.core.notifications import NotificationHandler
 import sendgrid
 from sendgrid.helpers.mail import Mail
 import os
+from app.core.exceptions import AppException, HTTPException
 
 
 class EmailNotification(NotificationHandler):
     email_parameters: dict
 
     def send(self):
-        sg = sendgrid.SendGridAPIClient(
-            api_key=os.environ.get('SENDGRID_API_KEY'))
-        msg = Mail(**self.email_parameters)
-        sg.send(msg)
+        try:
+            sg = sendgrid.SendGridAPIClient(
+                api_key=os.environ.get('SENDGRID_API_KEY'))
+            msg = Mail(**self.email_parameters)
+            sg.send(msg)
+        except Exception as e:
+            raise AppException.BadRequest(context=e.args[0])
