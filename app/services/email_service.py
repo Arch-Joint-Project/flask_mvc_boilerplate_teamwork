bf9f8f399934532f609025babf6731e44f381a45
Michael Asumadu <michaelasumadu1@gmail.com>
2021-10-15 09:06:10 +0000

    code refactoring

--- app/services/email_service.py
+++ app/services/email_service.py
@@ -1,20 +1,9 @@
 from app.core.notifications import NotificationHandler
-from app.core.exceptions import AppException
-
 
 
 class EmailNotification(NotificationHandler):
     email_parameters: dict
-    # sg: sendgrid
-    # msg: Mail
-
-    # def __init__(self):
-    #     self.sg = sendgrid.SendGridAPIClient(api_key=os.environ.get('SENDGRID_API_KEY'))
-    #     # self.msg = Mail(**self.email_parameters)
 
     def send(self):
         from app.utils.task import task_send_email
-        try:
-            task_send_email.delay(self.email_parameters)
-        except Exception as e:
-            raise AppException.OperationError(context=e.args[0])
+        task_send_email.delay(self.email_parameters)
